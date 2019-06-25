563959bfcf04b0130b3572d514ec0128bf3a766a
Tania Tushkyn <tania yushkyn@rivne.itstep.org>
2019-06-25 11:48:48 +0300

    Doble and graphica

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,53 +1,120 @@
-#include<iostream>
-#include<ctime>
+#include <iostream>
+#include <ctime>
 
 using namespace std;
-int main()
-{
+
+int main() {
 	srand(unsigned(time(NULL)));
+
 	char playerName[32];
 	char playerAI[] = "Vatson";
-
-	int playerScore = 0, aiScore = 0;
-	int playerDiceOne = 0, plaerDiceSecont = 0;
-	int aiDiceOne = 0, aiDiceSecond = 0;
-	int aiDiceSum = 0;
+	int playerScore = 0;
+	int aiScore = 0;
+	int playerDiceOne = 0;
+	int playerDiceSecond = 0;
 	int playerDiceSum = 0;
-
-
+	int aiDiceOne = 0;
+	int aiDiceSecond = 0;
+	int aiDiceSum = 0;
 	int n = 0;
 	bool exit = false;
 
-	cout << "<<<============================DICE GAME===================\n";
-	cout << "<<<=================                       ================\n";
-	cout << "<<<==============                                     =====\n";
-	cout << "<<<================                             ===========\n";
-	cout << "<<<=====                                               == =\n";
-	cout << "<<<==========                             =================\n";
-	cout << "<<<============                               =============\n";
-	cout << "<<<============================DICE GAME===================\n";
 
-	cout << "Enter your name = = = =" << endl;
+	cout << "<<<=============================Dice Game===========================>>>" << endl;
+	cout << "===========================         *         ===========================" << endl;
+	cout << "===========================         *         ===========================" << endl;
+	cout << "===========================         *         ===========================" << endl;
+	cout << "============================        *        ============================" << endl;
+	cout << "=============================       *       =============================" << endl;
+	cout << "==============================      *      ==============================" << endl;
+	cout << "======================              *               =====================" << endl;
+	cout << "========================            *             =======================" << endl;
+	cout << "=============================       *        ============================" << endl;
+	cout << "===============================     *      ==============================" << endl;
+	cout << "<<<=============================Dice Game============================>>>" << endl;
+
+	cout << "Enter your name = = = = >>>" << endl;
 	cin >> playerName;
-	cout << "Enter score to win" << endl;
+	cout << "Enter scores to win! " << endl;
 	cin >> n;
 	cout << playerName << " VS " << playerAI << endl;
 
+	cout << "---------------->>>Fight<<<-----------------" << endl;
 
-
-
-	cout << "--------------------->>>Start<<<--------------------------" << endl;
-	while (!exit)
-	{
-		cout << playerAI << " throw...." << endl;
-		aiDiceOne = rand() % 6 + 1;
-		aiDiceSecond = rand()%6 + 1;
-		aiDiceSum = aiDiceOne + aiDiceSecond;
-		cout << "First"<<aiDiceOne<<" Second "<<aiDiceSecond<<"Suma="<<aiDiceSum << endl;
-		cout << " Your throw..." << endl;
+	while (!exit) {
+		cout << playerAI << " $$$$$$ throw ... " << endl;
 		system("pause");
-		cout << playerName << " throw..."<< endl;
-		exit = true;
+		aiDiceOne = rand() % 6 + 1;
+		aiDiceSecond = rand() % 6 + 1;
+		aiDiceSum = aiDiceOne + aiDiceSecond;
+		if (aiDiceOne == aiDiceSecond)
+		{
+			aiDiceSum += 2;
+
+
+		}
+
+
+
+		cout << "First = " << aiDiceOne << " Second = " << aiDiceSecond << " Sum = " << aiDiceSum << endl;
+
+		cout << playerName << " ****** throw ... " << endl;
+		system("pause");
+
+		cout << playerName << " throw ... " << endl;
+		playerDiceOne = rand() % 6 + 1;
+		playerDiceSecond = rand() % 6 + 1;
+		playerDiceSum = playerDiceOne + playerDiceSecond;
+
+
+		if (playerDiceOne == playerDiceSecond)
+		{
+
+			playerDiceSum += 2;
+
+		}
+
+
+		cout << "First = " << playerDiceOne << " Second = " << playerDiceSecond << " Sum = " << playerDiceSum << endl;
+
+		if (aiDiceSum > playerDiceSum) {
+			aiScore++;
+
+		}
+		else if (aiDiceSum < playerDiceSum) {
+			playerScore++;
+		}
+		else {
+			cout << " DRAW " << endl;
+		}
+
+		/* if (aiDiceOne == aiDiceSecond)
+		{
+			 aiDiceSum+=  2;
+		
+
+		*/
+
+/*if (playerDiceOne == playerDiceSecond)
+		 {
+			
+			 playerDiceSum += 2;
+
+		 }*/
+
+		cout << "###################Throw result##############" << endl;
+		cout << playerAI << " Score = " << aiScore << endl;
+		cout << playerName << " Score = " << playerScore << endl;
+		cout << "-----------------------------------------------------\n\n";
+
+
+		if (playerScore == n) {
+			exit = true;
+		}
+		else if (aiScore == n) {
+			exit = true;
+		}
+
 	}
 
 	system("pause");
