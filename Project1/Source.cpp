569655b866baee2f1bc7d53438cbbc0952ea4059
Tania Tushkyn <tania yushkyn@rivne.itstep.org>
2019-06-25 10:09:55 +0300

    Creted skeleton

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,10 +3,36 @@
 using namespace std;
 int main()
 {
+	char playerName[32];
+	char playerAI[] = "Vatson";
+	int n = 0;
+	bool exit = false;
+
+	cout << "<<<============================DICE GAME===================\n";
+	cout << "<<<=================                       ================\n";
+	cout << "<<<==============                                     =====\n";
+	cout << "<<<================                             ===========\n";
+	cout << "<<<=====                                               == =\n";
+	cout << "<<<==========                             =================\n";
+	cout << "<<<============                               =============\n";
+	cout << "<<<============================DICE GAME===================\n";
+
+	cout << "Enter your name = = = =" << endl;
+	cin >> playerName;
+	cout << "Enter score to win" << endl;
+	cin >> n;
+	cout << playerName << " VS " << playerAI << endl;
 
 
 
 
+	cout << "--------------------->>>Start<<<--------------------------" << endl;
+	while (!false)
+	{
+
+		cout << "WHILE" << endl;
+		exit = false;
+	}
 
 	system("pause");
 	return 0;
